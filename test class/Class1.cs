e7348eb951fda1abeb06c61afa518aee1f7aa96a
shady-afkk <156012612+shady-afkk@users.noreply.github.com>
2024-02-12 19:15:02 -0500

    Add project files.

---
+++ test class/Class1.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework.Legacy;
+using NUnit.Framework;
+using static triangle_test.Class1;
+
+namespace test_class
+{
+    public class Class1
+    {
+        //Mohammed Shadaab Uddin
+
+        [TestFixture]
+        public class class1
+
+        {
+            [Test]
+            public void ValidTriangle_Input60and60_outputvalidTriangle()
+            {
+                //Arrange
+                int firstAngle = 60;
+                int secondAngle = 60;
+                int thirdAngle = 60;
+
+                string expected = "The triangle is valid.";
+
+                //Act
+                string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+                //Assert
+                ClassicAssert.AreEqual(expected, actual);
+            }
+        }    
+    }
+    
+}
---
+++ triangle test/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace triangle_test
+{
+    public class Class1
+    {
+        public static class Triangle
+        {
+            public static string ValidTriangle(int firstAngle, int secondAngle, int
+            thirdAngle)
+            {
+                string result;
+                if ((firstAngle + secondAngle + thirdAngle) == 180)
+                {
+                    result = "The triangle is valid.";
+                }
+                else
+                {
+                    result = "The triangle is NOT valid.";
+                }
+                return result;
+            }
+        }
+    }
+}
+
